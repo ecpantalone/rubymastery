e75a0219de50a336b9243b5ce1159774eb050937
ecpantalone <ecpantalone@gmail.com>
2020-09-29 12:04:21 -0400

    chapter 3

---
+++ ch3Letters/strings.rb
@@ -0,0 +1,17 @@
+puts "Hello, world!"
+puts ""
+puts "Good-bye."
+
+puts "I like" + "apple pie."
+
+puts "I like " + "apple pie."
+puts "I like" + " apple pie."
+
+puts "blink " * 4
+
+puts "12".to_i + 24
+
+puts "you\'re swell"
+puts "backslash at the end of a string: \\"
+puts "up\\down"
+puts "up\down"
