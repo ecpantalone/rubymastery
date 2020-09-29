1a3435a36c4077865da0d1c98a2fa7bff486f09b
ecpantalone <ecpantalone@gmail.com>
2020-09-29 14:38:56 -0400

    chapter 4

---
+++ ch4Variables/assignments.rb
@@ -0,0 +1,20 @@
+my_string = '...you can say that again ...'
+puts my_string
+puts my_string
+
+my_own_var = 'just another ' + 'string'
+puts my_own_var
+
+my_own_var = 5 * (1+3)
+puts my_own_var
+
+var1 = 8
+var2 = var1
+puts var1
+puts var2
+
+puts ''
+
+var1 = 'eight'
+puts var1
+puts var2
